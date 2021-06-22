2caae1ca429119eb2f10223364d74e8f14ee2d02
muliawan021 <muliawan021@gmail.com>
2021-06-22 11:43:33 +0800

    test

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return 'Selamat Belajar DevNet v 1'
 
+@app.route('/test')
+def test():
+    return 'ini halaman uji coba dengan menambahkan /test'
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000")
